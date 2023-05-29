fbecb93f2cda67eeb5b2f545a7465db0b23eab4e
Rocky971 <rkrocky2018@gmail.com>
2023-05-29 20:18:26 +0600

    all page completed

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// contact page js accordtion with
+// accordion 1
+document.getElementById("Ac1").addEventListener("click", function () {
+    const element = document.getElementById("Acp1");
+    element.classList.toggle("hidden");
+});
+// accordion 2
+document.getElementById("Ac2").addEventListener("click", function () {
+    const element = document.getElementById("Acp2");
+    element.classList.toggle("hidden");
+});
+// accordion 3
+document.getElementById("Ac3").addEventListener("click", function () {
+    const element = document.getElementById("Acp3");
+    element.classList.toggle("hidden");
+});
+// accordion 4
+document.getElementById("Ac4").addEventListener("click", function () {
+    const element = document.getElementById("Acp4");
+    element.classList.toggle("hidden");
+});
